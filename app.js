891a57039389e622cf9bf5a4e8c6b9479b041485
Nilisha Jaiswal <73216246+nilisha-jais@users.noreply.github.com>
2021-03-27 09:02:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 (function($) { "use strict";
  
 $(function() {
@@ -45,3 +44,19 @@ $("#switch").on('click', function () {
 });  
 
 })(jQuery);
+
+const barOuter = document.querySelector(".bar-outer");
+const options = document.querySelectorAll(".bar-grey .option");
+let current = 1;
+options.forEach((option, i) => (option.index = i + 1));
+options.forEach(option =>
+                option.addEventListener("click", function() {
+  barOuter.className = "bar-outer";
+  barOuter.classList.add(`pos${option.index}`);
+  if (option.index > current) {
+    barOuter.classList.add("right");
+  } else if (option.index < current) {
+    barOuter.classList.add("left");
+  }
+  current = option.index;
+}));
